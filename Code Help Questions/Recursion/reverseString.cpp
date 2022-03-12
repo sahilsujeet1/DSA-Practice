c77f5492d22b5dd578a5bf29d239d0ee2dfe1e62
Sahil <sahilsujeet1@gmail.com>
2022-03-12 22:48:00 +0530

    reverseString using recursion and single pointer

--- Code Help Questions/Recursion/reverseString.cpp
+++ Code Help Questions/Recursion/reverseString.cpp
@@ -9,11 +9,20 @@ void reverseString(string &str, int s, int e) {
     reverseString(str, ++s, --e);
 }
 
+void reverseString1(string &str, int i) {
+    if(i > str.length()-1-i)
+        return;
+    
+    swap(str[i], str[str.length()-1-i]);
+    reverseString1(str, ++i);
+}
+
 int main() {
     string str = "Sahil";
     
     cout << "Original String: " << str << endl;
     
-    reverseString(str, 0, str.length()-1);
+    // reverseString(str, 0, str.length()-1);
+    reverseString1(str, 0);
     cout << "Reversed String: " << str << endl;
 }
