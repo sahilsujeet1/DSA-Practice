a7517fbd8abf1429ae9a3a9812a222a045e045a2
Sahil <sahilsujeet1@gmail.com>
2022-03-17 00:44:33 +0530

    subsetsOfArray using bit masking

--- Code Help Questions/Recursion/subsetsOfArray.cpp
+++ Code Help Questions/Recursion/subsetsOfArray.cpp
@@ -1,19 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void subsets(vector<int> arr, vector<int> output, int i, vector<vector<int>>& ans) {
+void subsetsRecursion(vector<int> arr, vector<int> output, int i, vector<vector<int>>& ans) {
     if(i >= arr.size()) {
         ans.push_back(output);
         return;
     }
 
     // Exclude
-    subsets(arr, output, i+1, ans);
+    subsetsRecursion(arr, output, i+1, ans);
 
     // Include
     output.push_back(arr[i]);
-    subsets(arr, output, i+1, ans);
+    subsetsRecursion(arr, output, i+1, ans);
 }
+
+vector<vector<int>> subsetsBitMasking(vector<int>& nums) {
+        int size = nums.size();
+        int subsetSize = (1 << size);
+        vector<vector<int>> ans;
+        
+        for(int subset = 0; subset < subsetSize; subset++) {
+            vector<int> subs;
+            for(int i = 0; i < size; i++) {
+                if((subset & (1 << i)) != 0) {
+                    subs.push_back(nums[i]);
+                }
+            }
+            ans.push_back(subs);
+        }
+        
+        return ans;
+    }
 
 int main() {
     vector<int> arr = {1,2,3,4};
@@ -21,8 +39,8 @@ int main() {
     vector<vector<int>> ans;
     vector<int> output;
 
-    subsets(arr, output, 0, ans);    
-
+    // subsetsRecursion(arr, output, 0, ans);    
+    ans = subsetsBitMasking(arr);
     cout << endl << "Output: " << endl;
     for(auto x:ans) {
         for(auto y: x) {
