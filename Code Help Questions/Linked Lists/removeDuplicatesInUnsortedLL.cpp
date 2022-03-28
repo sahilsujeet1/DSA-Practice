827049578d121d2a35bca5093b8536403540f912
Sahil <sahilsujeet1@gmail.com>
2022-03-28 23:56:37 +0530

    remove duplicate using unordered set

--- Code Help Questions/Linked Lists/removeDuplicatesInUnsortedLL.cpp
+++ Code Help Questions/Linked Lists/removeDuplicatesInUnsortedLL.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include "Node.cpp"
 using namespace std;
 
+// Using two loops
+// Time complexity: O(n^2)      Space complexity: O(1)
 void removeDuplicate1(Node *head) {
     if(head == NULL)    
         return;
@@ -27,6 +29,36 @@ void removeDuplicate1(Node *head) {
     }
 }
 
+// Using unordered set
+// Time Complexity: O(n)        Space Complexity: O(n)
+void removeDuplicate2(Node *head) {
+    if(head == NULL)
+        return;
+    
+    unordered_set<int> visited;
+    Node *curr = head;
+    Node *prev = NULL;
+
+    while(curr != NULL) {
+        if(visited.find(curr->data) != visited.end()) {
+            Node *toDelete = curr;
+            prev->next = curr->next;
+            curr = curr->next;
+            delete toDelete;
+        } else {
+            visited.insert(curr->data);
+            prev = curr;
+            curr = curr->next;
+        }
+    }
+}
+
+// Using merge sort to sort and then remove duplicate from sorted LL
+// Time Complexity: O(nlogn)        Space Complexity: O(1)
+void removeDuplicate(Node *head) {
+
+}
+
 int main() {
     Node *node = new Node(2);
     Node *head = node;
@@ -40,7 +72,7 @@ int main() {
     cout << "Original Linked List: ";
     print(node);
 
-    removeDuplicate1(node);
+    removeDuplicate2(node);
     cout << "Linked list without duplicates: ";
     print(node);
 }
