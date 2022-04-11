2fec4781622043a08831977eff16cb54b5e88655
Sahil <sahilsujeet1@gmail.com>
2022-04-11 23:10:16 +0530

    queue implementation

---
+++ Code Help Questions/Queues/implementation.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+
+class Queue {
+    
+    int* arr;
+    int qfront;
+    int rear; 
+    int size;
+    
+public:
+    Queue() {
+        size = 100001;
+        arr = new int[size];
+        qfront = 0;
+        rear = 0;
+    }
+
+
+    bool isEmpty() {
+        if(qfront == rear) {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void enqueue(int data) {
+        if(rear == size)
+            cout << "Queue is Full" << endl;
+        else
+        {
+            arr[rear] = data;
+            rear++;
+        }
+    }
+
+    int dequeue() {
+        if(qfront == rear) {
+            return -1;
+        }
+        else
+        {	int ans = arr[qfront];
+            arr[qfront] = -1;
+            qfront++;
+            if(qfront==rear){
+                qfront = 0;
+                rear = 0;
+            }
+         return ans;
+        }
+    }
+
+    int front() {
+        if(qfront == rear) {
+            return -1;
+        }
+        else
+        {
+            return arr[qfront];
+        }
+    }
+};
+
+int main() {
+    Queue q;
+
+    q.enqueue(5);
+    cout << q.front() << endl;
+
+    q.enqueue(2);
+
+    q.enqueue(3);
+
+    q.dequeue();
+    cout << q.front() << endl;
+
+    q.dequeue();
+    cout << q.front() << endl;
+
+    cout << "Is empty: " << q.isEmpty() << endl;
+}
