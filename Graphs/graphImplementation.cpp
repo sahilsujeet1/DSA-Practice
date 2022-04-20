f0a2438cef0cc24d1e1ac076802323409d9cd6c8
Sahil <sahilsujeet1@gmail.com>
2022-04-21 00:06:26 +0530

    adjacency matrix graph representation

--- Graphs/graphImplementation.cpp
+++ Graphs/graphImplementation.cpp
@@ -1,13 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 void addEdgeList(vector<int> adj[], int u, int v) {
     adj[u].push_back(v);
     adj[v].push_back(u);
 }
 
+void addEdgeMatrix(vector<vector<int>> &mat, int u, int v) {
+    mat[u][v] = 1;
+    mat[v][u] = 1;
+}
+
+void printMatrix(vector<vector<int>> &mat) {
+    cout << "\nAdjacency Matrix Graph Representation" << endl << endl;
+
+    cout << "    ";
+    for(int i=0; i<mat.size(); i++) 
+        cout << i << " ";
+    cout << "\n    ";
+    for(int i=0; i<mat.size(); i++) 
+        cout << "-" << " ";
+    cout << endl;
+
+    for(int i=0; i<mat.size(); i++) {
+        cout << i << " | ";
+        for(int j=0; j<mat[i].size(); j++) {
+            cout << mat[i][j] << " ";
+        } cout << endl;
+    }
+}
 void printList(vector<int> adj[], int n) {
-    cout << "Adjacency List Graph Representation" << endl;
+    cout << endl << endl << "Adjacency List Graph Representation" << endl;
 
     for(int i=0; i<n; i++) {
         cout << i << " : ";
@@ -35,4 +57,22 @@ int main() {
     addEdgeList(adj, 3,4);
     
     printList(adj, n);
+
+    vector<vector<int>> matrix;
+    for(int i=0; i<n; i++) {
+        vector<int> row;
+        for(int j=0; j<n; j++) {
+            row.push_back(0);
+        }
+        matrix.push_back(row);
+    }
+
+    addEdgeMatrix(matrix, 0,1);
+    addEdgeMatrix(matrix, 0,4);
+    addEdgeMatrix(matrix, 1,2);
+    addEdgeMatrix(matrix, 2,3);
+    addEdgeMatrix(matrix, 2,4);
+    addEdgeMatrix(matrix, 3,4);
+
+    printMatrix(matrix);
 }
