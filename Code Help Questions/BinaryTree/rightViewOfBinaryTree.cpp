2cb26b559cb87a0c50db5231d4e9e7d469c80d9e
Sahil <sahilsujeet1@gmail.com>
2022-04-29 14:05:09 +0530

    right view of binary tree

---
+++ Code Help Questions/BinaryTree/rightViewOfBinaryTree.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include "node.cpp"
+using namespace std;
+
+class Solution
+{
+    public:
+    //Function to return list containing elements of right view of binary tree.
+    void solve(Node *root, vector<int> &ans, int lvl) {
+        if(root == NULL)
+            return;
+            
+        if(lvl == ans.size())
+            ans.push_back(root->data);
+            
+        solve(root->right, ans, lvl+1);
+        solve(root->left, ans, lvl+1);
+        
+    }
+
+    vector<int> rightView(Node *root)
+    {
+       // Your code here
+        vector<int> ans;
+        if(root == NULL)
+            return ans;
+        
+        solve(root, ans, 0);
+        return ans;
+    }
+};
