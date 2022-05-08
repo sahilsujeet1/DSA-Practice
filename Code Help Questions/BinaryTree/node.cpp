15b0559f36631e5783017b0d79110fea080f2f7f
Sahil <sahilsujeet1@gmail.com>
2022-05-08 22:17:23 +0530

    added traversals in Binary tree node file

--- Code Help Questions/BinaryTree/node.cpp
+++ Code Help Questions/BinaryTree/node.cpp
@@ -13,6 +13,62 @@ class Node {
     }
 };
 
+void preorderTraversal(Node *root) {
+    if(root == NULL)
+        return;
+    
+    cout << root->data << " ";
+    preorderTraversal(root->left);
+    preorderTraversal(root->right);
+}
+
+void inorderTraversal(Node *root) {
+    if(root == NULL)
+        return;
+    
+    inorderTraversal(root->left);
+    cout << root->data << " ";
+    inorderTraversal(root->right);
+}
+
+void postorderTraversal(Node *root) {
+    if(root == NULL)
+        return;
+    
+    postorderTraversal(root->left);
+    postorderTraversal(root->right);
+    cout << root->data << " ";
+}
+
+void levelOrderTraversal(Node *root) {
+    if(root) {
+        queue<Node*> q;
+        q.push(root);
+        q.push(NULL);
+
+        while(!q.empty()) {
+            Node *temp = q.front();
+            q.pop();
+
+            if(temp == NULL) {
+                cout << endl; // traversed last level completely
+                if(!q.empty()) {
+                    q.push(NULL); //queue still has some nodes
+                }
+            } else {
+                cout << temp->data << " ";
+
+                if(temp->left)
+                    q.push(temp->left);
+                if(temp->right)
+                    q.push(temp->right);
+            }
+        }
+    } else {
+        cout << "Tree root is unavailable" << endl;
+    }
+}
+
 void buildFromLevelOrder(Node* &root) {
     cout << "Enter root value: ";
     int data;
