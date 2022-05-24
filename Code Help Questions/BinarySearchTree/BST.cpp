ada2e3569ad81fe79211b8ab400d05c73e51b59f
Sahil <sahilsujeet1@gmail.com>
2022-05-24 22:40:53 +0530

    BST insertion

---
+++ Code Help Questions/BinarySearchTree/BST.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include "node.cpp"
+using namespace std;
+
+Node *insertIntoBST(Node *root, int val) {
+    if(root == NULL) {
+        root = new Node(val);
+        return root;
+    }
+
+    if(root->data < val) {
+        root->right = insertIntoBST(root->right, val);
+    } else {
+        root->left = insertIntoBST(root->left, val);
+    }
+
+    return root;
+}
+
+void takeInput(Node* &root) {
+    int data;
+    cin >> data;
+
+    while(data != -1) {
+        root = insertIntoBST(root, data);
+        cin >> data;
+    }
+
+}
+
+int main() {
+    Node *root = NULL;
+
+    cout << endl << "Enter nodes to insert into BST: ";
+    takeInput(root);
+
+    cout << endl << "Level order: " << endl;
+    levelOrderTraversal(root);
+}
