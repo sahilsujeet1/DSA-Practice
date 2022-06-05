1026ee43e21e526b732a9547a8cffe51b0e302fb
Sahil <sahilsujeet1@gmail.com>
2022-06-05 23:16:42 +0530

    merge2BST Approach2 not working yet

--- Code Help Questions/BinarySearchTree/merge2BST.cpp
+++ Code Help Questions/BinarySearchTree/merge2BST.cpp
@@ -54,6 +54,149 @@ class Approach1 {
 };
 
 
+class Approach2 {
+  public:
+    void BSTtoDLL(Node *root, Node* &head) {
+        if(root == NULL)
+            return;
+        
+        BSTtoDLL(root->right, head);
+        
+        root->right = head;
+        if(head != NULL)
+            head->right = root;
+        head = root;
+        
+        BSTtoDLL(root->left, head);
+    }
+    
+//      Node *mergeLogic(Node  *first, Node  *second) {
+
+//         if(first->right == NULL) {
+//             first->right = second;
+//             return first;
+//         }
+
+//         Node *curr1 = first;
+//         Node *next1 = curr1->right;
+//         Node *curr2 = second;
+//         Node *next2 = curr2->right;
+
+//         while(next1 != NULL && curr2 != NULL) {
+//             if((curr2->data >= curr1->data) && (curr2->data <= next1->data)) {
+//                 curr1->right = curr2;
+//                 next2 = curr2->right;
+//                 curr2->right = next1;
+
+//                 curr1 = curr2;
+//                 curr2 = next2;
+
+//             } else {
+//                 curr1 = curr1->right;
+//                 next1 = next1->right;
+
+//                 if(next1 == NULL) {
+//                     curr1->right = curr2;
+//                     return first;
+//                 }
+//             }
+//         }
+
+//         return first;
+//     }
+    
+    Node *mergeTwoSortedDLL(Node *head1, Node *head2) {
+        Node *head = NULL;
+        Node *tail = NULL;
+        
+//         if(head1->data < head2->data) {
+//             return mergeLogic(head1, head2);
+//         } else {
+//             return mergeLogic(head2, head1);
+//         }
+        
+        while(head1 != NULL && head2 != NULL) {
+            if(head1->data < head2->data) {
+                if(head == NULL) {
+                    head = head1;
+                } else {
+                    tail->right = head1;
+                    head1->left = tail;
+                }
+                tail = head1;
+                head1 = head1->right;
+            } else {
+                if(head == NULL) {
+                    head = head2;
+                } else {
+                    tail->right = head2;
+                    head2->left = tail;
+                }
+                tail = head2;
+                head2 = head2->right;
+            }
+        }
+        
+        while(head1 != NULL) {
+            tail->right = head1;
+            head1->left = tail;
+            tail = head1;
+            head1 = head1->right;
+        }
+        
+        while(head2 != NULL) {
+            tail->right = head2;
+            head2->left = tail;
+            tail = head2;
+            head2 = head2->right;
+        }
+        
+        return head;
+    }
+    
+    int countNodes(Node *root) {
+        int cnt = 0;
+        Node *temp = root;
+        while(temp != NULL) {
+            cnt++;
+            temp = temp->right;
+        }
+        return cnt;
+    }
+    
+    Node *DLLtoBST(Node* &root, int n) {
+        if(n <= 0 || root == NULL)
+            return NULL;
+        
+        Node *left = DLLtoBST(root->left, n/2);
+        Node *head = root;
+        head->left = left;
+        
+        root = root->right;
+        
+        Node *right = DLLtoBST(root->right, n/2-1);
+        head->right = right;
+        
+        return head;
+    }
+        
+    Node *findAns(Node *root1, Node *root2) {
+        Node *ll1 = NULL;
+        BSTtoDLL(root1, ll1);
+        
+        Node *ll2 = NULL;
+        BSTtoDLL(root2, ll2);
+        
+        Node *mergedDLL = mergeTwoSortedDLL(ll1, ll2);
+        mergedDLL->left = NULL;
+        int n = countNodes(mergedDLL);
+        
+        return DLLtoBST(mergedDLL, n);
+        return NULL;
+    }
+};
+
+
 Node *mergeBST(Node *root1, Node *root2){
     // Write your code here.
     Approach1 obj1;
