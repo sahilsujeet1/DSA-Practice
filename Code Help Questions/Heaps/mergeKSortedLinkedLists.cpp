0fedc0c68980801c31932d85927d6d94ec958074
Sahil <sahilsujeet1@gmail.com>
2022-06-17 12:03:10 +0530

    merge k sorted linked lists using heaps

---
+++ Code Help Questions/Heaps/mergeKSortedLinkedLists.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+    class Node {
+        public:
+        T data;
+        Node* next;
+
+        Node(T data) {
+            next = NULL;
+            this->data = data;
+        }
+
+        ~Node() {
+            if (next != NULL) {
+                delete next;
+            }
+        }
+    };
+
+class compare {
+  public:
+    bool operator()(Node<int>* a, Node<int>* b) {
+        return a->data > b->data;
+    }
+};
+
+Node<int>* mergeKLists(vector<Node<int>*> &listArray)
+{
+    // Write your code here.
+    priority_queue<Node<int>*, vector<Node<int>*>, compare> minHeap;
+    int k = listArray.size();
+    
+    if(k == 0) return NULL;
+    
+    for(int i=0; i<k; i++) {
+        if(listArray[i] != NULL)
+            minHeap.push(listArray[i]);
+    }
+    
+    Node<int>* head = NULL;
+    Node<int>* tail = NULL;
+    
+    while(!minHeap.empty()) {
+        Node<int>* top = minHeap.top();
+        minHeap.pop();
+        
+        if(top->next != NULL) {
+            minHeap.push(top->next);
+        }
+        
+        if(head == NULL) {
+            head = tail = top;
+        } else {
+            tail->next = top;
+            tail = top;
+        }
+    }
+    
+    return head;
+}
