da34b1af8522848f76db53e7161945b9912440f1
Sahil <sahilsujeet1@gmail.com>
2022-07-22 23:00:58 +0530

    Bellman Ford Shortest Distance Algorithm

---
+++ Code Help Questions/Graphs/bellManFordShortestDistance.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int bellmonFord(int n, int m, int src, int dest, vector<vector<int>> &edges) {
+    // Write your code here.
+    vector<int> dist(n+1, 1e9);
+    dist[src] = 0;
+    
+    for(int i=1; i<=n; i++) {
+        for(int j=0; j<m; j++) {
+            int u = edges[j][0];
+            int v = edges[j][1];
+            int wt = edges[j][2];
+            
+            if((dist[u] != 1e9) && ((dist[u] + wt) < dist[v])) {
+                dist[v] = dist[u] + wt;
+            }
+        }
+    }
+        
+    bool flag = false;
+    for(int i=0; i<m; i++) {
+        int u = edges[i][0];
+        int v = edges[i][1];
+        int wt = edges[i][2];
+
+        if(dist[u] != 1e9 && (dist[u] + wt < dist[v])) {
+            flag = true;
+        }
+    }
+    
+    if(flag)
+        return -1;
+    return dist[dest];
+    
+}
