57c166e625719152a0479773a7c7e33296f332b0
Sahil <sahilsujeet1@gmail.com>
2022-07-30 16:14:07 +0530

    minElement using tabulation

--- Code Help Questions/DynamicProgramming/minElementToCreateTarget.cpp
+++ Code Help Questions/DynamicProgramming/minElementToCreateTarget.cpp
@@ -34,3 +34,33 @@ int solveMem(vector<int> &num, int x, vector<int> &dp) {
     dp[x] = mini;
     return dp[x];
 }
+
+int solveTab(vector<int> &num, int x) {
+    vector<int> dp(x+1, INT_MAX);
+    dp[0] = 0;
+    
+    for(int i=1; i<=x; i++) {
+        for(int j=0; j<num.size(); j++) {
+            if(i-num[j] >=0 && dp[i-num[j]] != INT_MAX) {
+                dp[i] = min(dp[i], 1+dp[i-num[j]]);
+            }
+        }
+    }
+    
+    if(dp[x] == INT_MAX)
+        return -1;
+    return dp[x];
+}
+
+int minimumElements(vector<int> &num, int x)
+{
+    // Write your code here.
+//     int ans = solveRec(num, x);
+    vector<int> dp(x+1, -1);
+//     int ans = solveMem(num, x, dp);
+    int ans = solveTab(num, x);
+
+    if(ans == INT_MAX)
+        return -1;
+    return ans;
+}
