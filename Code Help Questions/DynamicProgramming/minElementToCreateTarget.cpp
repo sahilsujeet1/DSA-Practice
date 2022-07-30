db417788257051007b90c64dd3a59283456324b3
Sahil <sahilsujeet1@gmail.com>
2022-07-30 16:13:25 +0530

    minElement using memoisation

--- Code Help Questions/DynamicProgramming/minElementToCreateTarget.cpp
+++ Code Help Questions/DynamicProgramming/minElementToCreateTarget.cpp
@@ -15,3 +15,22 @@ int solveRec(vector<int> &num, int x) {
     }
     return mini;
 }
+
+int solveMem(vector<int> &num, int x, vector<int> &dp) {
+    if(x == 0) return 0;
+    if(x < 0) return INT_MAX;
+    
+    if(dp[x] != -1)
+        return dp[x];
+    
+    int mini = INT_MAX;
+    for(int i=0; i<num.size(); i++) {
+        int ans = solveMem(num, x-num[i], dp);
+        
+        if(ans != INT_MAX)
+            mini = min(mini, ans+1);
+    }
+    
+    dp[x] = mini;
+    return dp[x];
+}
