6c376855c23103cbe29d4265d01950ddfc0312d6
Sahil <sahilsujeet1@gmail.com>
2022-08-03 14:27:31 +0530

    minCostClimbingStairs sol3 using tabulation

--- Code Help Questions/DynamicProgramming/minCostClimbingStairs.cpp
+++ Code Help Questions/DynamicProgramming/minCostClimbingStairs.cpp
@@ -16,6 +16,18 @@ int solve2(vector<int> &cost, int n, vector<int> &dp) {
     if(dp[n] != -1)
         return dp[n];
 
-    dp[n] = cost[n] + min(solve1(cost, n-1, dp), solve1(cost, n-2, dp));
+    dp[n] = cost[n] + min(solve2(cost, n-1, dp), solve2(cost, n-2, dp));
     return dp[n];
+}
+
+int solve3(vector<int> &cost, int n) {
+    vector<int> dp(n+1);
+    dp[0] = cost[0];
+    dp[1] = cost[1];
+
+    for(int i=2; i<n; i++) {
+        dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
+    }
+
+    return min(dp[n-1], dp[n-2]);
 }
