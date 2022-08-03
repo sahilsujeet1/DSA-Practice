51463e1d950ce192932e21086a2cc9c218f4f57e
Sahil <sahilsujeet1@gmail.com>
2022-08-03 14:24:43 +0530

    minCostClimbingStairs sol2 using memoisation

--- Code Help Questions/DynamicProgramming/minCostClimbingStairs.cpp
+++ Code Help Questions/DynamicProgramming/minCostClimbingStairs.cpp
@@ -9,3 +9,13 @@ int solve1(vector<int> &cost, int n) {
     return ans;
 }
 
+int solve2(vector<int> &cost, int n, vector<int> &dp) {
+    if(n == 0) return cost[0];
+    if(n == 1) return cost[1];
+
+    if(dp[n] != -1)
+        return dp[n];
+
+    dp[n] = cost[n] + min(solve1(cost, n-1, dp), solve1(cost, n-2, dp));
+    return dp[n];
+}
