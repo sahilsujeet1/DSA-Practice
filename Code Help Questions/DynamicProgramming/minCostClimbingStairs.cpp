a669362b82e5b9f044080e1715ac676805e426bd
Sahil <sahilsujeet1@gmail.com>
2022-08-04 14:29:24 +0530

    minCostClimbingStairs sol4 using tab & space opt

--- Code Help Questions/DynamicProgramming/minCostClimbingStairs.cpp
+++ Code Help Questions/DynamicProgramming/minCostClimbingStairs.cpp
@@ -31,3 +31,17 @@ int solve3(vector<int> &cost, int n) {
 
     return min(dp[n-1], dp[n-2]);
 }
+
+int solve4(vector<int> &cost, int n) {
+    vector<int> dp(n+1);
+    int prev2 = cost[0];
+    int prev1 = cost[1];
+
+    for(int i=2; i<n; i++) {
+        int curr = cost[i] + min(prev1, prev2);
+        prev1 = prev2;
+        prev2 = curr;
+    }
+
+    return min(prev1, prev2);
+}
