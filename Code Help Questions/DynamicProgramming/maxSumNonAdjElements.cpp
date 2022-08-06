3f30f7e810e6f3063c8f4efe3764f6fdb8becc25
Sahil <sahilsujeet1@gmail.com>
2022-08-06 23:59:42 +0530

    maxSumNonAdjElements using tabulation & space opt

--- Code Help Questions/DynamicProgramming/maxSumNonAdjElements.cpp
+++ Code Help Questions/DynamicProgramming/maxSumNonAdjElements.cpp
@@ -39,3 +39,30 @@ int solveTab(vector<int> &nums) {
     
     return dp[n-1];
 }
+
+int solveSO(vector<int> &nums) {
+    int n = nums.size();
+    
+    int prev2 = 0;
+    int prev1 = nums[0];
+    
+    for(int i=1; i<n; i++) {
+        int inc = nums[i] + prev2;
+        int exc = prev1;
+        int ans = max(inc, exc);
+        prev2 = prev1;
+        prev1 = ans;
+    }
+    
+    return prev1;
+}
+
+int maximumNonAdjacentSum(vector<int> &nums){
+    // Write your code here.
+    int n = nums.size();
+    vector<int> dp(n, -1);
+//     return solveRec(nums, n-1);
+//     return solveMem(nums, n-1, dp);
+//     return solveTab(nums);
+    return solveSO(nums);
+}
